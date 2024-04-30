7efeb974264662cec6200bfe13ff0f71b7a33c54
shkum0330 <shkum0330@gmail.com>
2024-04-30 17:40:43 +0900

    20240430

---
+++ java-mid1/src/ch1/ex1/Rectangle.java
@@ -0,0 +1,29 @@
+package ch1.ex1;
+
+import java.util.Objects;
+
+public class Rectangle {
+    int width,height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+}
