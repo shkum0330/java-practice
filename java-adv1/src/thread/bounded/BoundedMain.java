8aa4d15b60d21a7a0d77d6da58fd5d698263f089
sehyun kum <shkum0330@gmail.com>
2024-08-28 11:39:37 +0900

    20240828

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -9,9 +9,8 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-//        BoundedQueue queue = new BoundedQueueV1(2);
-//        BoundedQueue queue = new BoundedQueueV2(2);
-        BoundedQueue queue = new BoundedQueueV3(2);
+        BoundedQueue queue = new BoundedQueueV6_4(2);
+
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
         producerFirst(queue); // 생산자 먼저 실행
 //        consumerFirst(queue); // 소비자 먼저 실행
