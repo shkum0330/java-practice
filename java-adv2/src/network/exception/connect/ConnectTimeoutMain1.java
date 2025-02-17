0a313dbff79fbccf0d1d9e8b98031fe1f45eb380
shkum0330 <shkum0330@gmail.com>
2025-02-17 23:51:10 +0900

    20250217

---
+++ java-adv2/src/network/exception/connect/ConnectTimeoutMain1.java
@@ -0,0 +1,19 @@
+package network.exception.connect;
+
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class ConnectTimeoutMain1 {
+    public static void main(String[] args) throws Exception{
+        long start = System.currentTimeMillis();
+        try {
+            Socket socket = new Socket("192.168.1.250", 45678);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("end = " + (end - start));
+
+        long endTime = System.currentTimeMillis();
+    }
+}
---
+++ java-adv2/src/network/exception/connect/ConnectTimeoutMain2.java
@@ -0,0 +1,21 @@
+package network.exception.connect;
+
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ConnectTimeoutMain2 {
+    public static void main(String[] args) throws Exception{
+        long start = System.currentTimeMillis();
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("192.168.1.250", 45678),1000);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("end = " + (end - start));
+
+        long endTime = System.currentTimeMillis();
+    }
+}
