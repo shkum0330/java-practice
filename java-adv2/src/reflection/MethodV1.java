8dbf23d62d50e7ef57fd41204cbb1c73bde9b50d
shkum0330 <shkum0330@gmail.com>
2025-04-30 17:38:21 +0900

    20250430

---
+++ java-adv2/src/reflection/MethodV1.java
@@ -0,0 +1,23 @@
+package reflection;
+
+import reflection.data.BasicData;
+
+import java.lang.reflect.Method;
+
+public class MethodV1 {
+    public static void main(String[] args) {
+        Class<BasicData> helloClass = BasicData.class;
+
+        System.out.println("====== methods() =====");
+        Method[] methods = helloClass.getMethods(); // 모든 public 메서드를 반환
+        for (Method method : methods) {
+            System.out.println("method = " + method);
+        }
+
+        System.out.println("====== declaredMethods() =====");
+        Method[] declaredMethods = helloClass.getDeclaredMethods(); // 해당 클래스에서 선언된 모든 메서드를 반환
+        for (Method method : declaredMethods) {
+            System.out.println("declaredMethods = " + method);
+        }
+    }
+}
