bd294763c52420864131ec579b6d0bc6e1462328
shkum0330 <shkum0330@gmail.com>
2025-05-04 14:47:23 +0900

    20250504

--- java-adv2/src/was/httpserver/HttpRequest.java
+++ java-adv2/src/was/httpserver/HttpRequest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.*;
+import static util.MyLogger.log;
 
 public class HttpRequest {
     private String method;
@@ -19,6 +20,28 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parseHeaders(reader);
+        parseBody(reader);
+    }
+
+    private void parseBody(BufferedReader reader) throws IOException {
+        if(!headers.containsKey("Content-Length")){
+            return;
+        }
+
+        int contentLength=Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars=new char[contentLength];
+        int read= reader.read(bodyChars);
+        if(read != contentLength){
+            throw new IOException("Failed to read entire body. Expected " + contentLength + " bytes, but read " + read);
+        }
+
+        String body=new String(bodyChars);
+        log("HTTP Message Body: " + body);
+
+        String contentType=headers.get("Content-Type");
+        if ("application/x-www-form-urlencoded".equals(contentType)) {
+            parseQueryParameters(body);
+        }
     }
 
     private void parseHeaders(BufferedReader reader) throws IOException {
